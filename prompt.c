6aceeb10000be063df0f783d4a62cc8946bdbd53
Elijah <waiganjothiongo12@gmail.com>
2023-11-01 01:22:57 -0700

    Task 2

--- prompt.c
+++ prompt.c
@@ -1,49 +1,60 @@
 #include "Functions.h"
+
 void prompt(char **av, char **env)
 {
-	char *string = NULL;
-	int i, j, status;
-	size_t n = 0;
-	ssize_t num_char;
-	char *argv[FUNCTION_H];
-	pid_t child_pid;
+    char *string = NULL;
+    int i, j, status;
+    size_t n = 0;
+    ssize_t num_char;
+    char *argv[FUNCTION_H];
+    pid_t child_pid;
 
-	while (1)
-	{
-		if(isatty(STDIN_FILENO))
-		printf("Shell$ ");
-		num_char = getline(&string, &n, stdin);
-		if (num_char == -1)
-		{
-			free(string);
-			exit(EXIT_FAILURE);
-		}
-		i = 0;
-		while(string[i])
-		{
-			if (string[i] == '\n')
-				string[i] = 0;
-			i++;
-		}
-		j = 0;
-		argv[j] = strtok(string, " ");
-		while(argv[j])
-		{
-			argv[++j] = strtok(NULL, " ");
-		}
-		child_pid = fork();
+    while (1)
+    {
+        if (isatty(STDIN_FILENO))
+            printf("Shell$ ");
+        num_char = getline(&string, &n, stdin);
+        if (num_char == -1)
+        {
+            free(string);
+            exit(EXIT_FAILURE);
+        }
+        i = 0;
+        while (string[i])
+        {
+            if (string[i] == '\n')
+                string[i] = 0;
+            i++;
+        }
+        j = 0;
+        argv[j] = strtok(string, " ");
+        while (argv[j])
+        {
+            argv[++j] = strtok(NULL, " ");
+        }
+        if (access(argv[0], X_OK) == 0)
+        {
+            child_pid = fork();
 
-		if (child_pid == -1)
-		{
-			free(string);
-			exit(EXIT_FAILURE);
-		}
-		if (child_pid == 0)
-		{
-			if (execve(argv[0], argv, env) == -1)
-				printf("%s: No such file or directory\n", av[0]);
-		}
-		else
-			wait(&status);
-	}
+            if (child_pid == -1)
+            {
+                free(string);
+                exit(EXIT_FAILURE);
+            }
+            if (child_pid == 0)
+            {
+                if (execve(argv[0], argv, env) == -1)
+                {
+                    perror("execve");
+                    exit(EXIT_FAILURE);
+                }
+            }
+            else
+                wait(&status);
+        }
+        else
+        {
+            printf("%s: No such file or directory\n", av[0]);
+        }
+    }
 }
