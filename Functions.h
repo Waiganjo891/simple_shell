2750aa4f46b5c04c7ca6eba9445e9f8a788b0351
Elijah <waiganjothiongo12@gmail.com>
2023-10-31 23:56:09 -0700

    Fix 1

---
+++ Functions.h
@@ -0,0 +1,12 @@
+#ifndef FUNCT_H
+#define FUNCTION_H 102
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+
+void prompt(char **av, char **env);
+
+#endif
---
+++ main.c
@@ -0,0 +1,7 @@
+#include "Functions.h"
+int main (int ac, char **av, char **env)
+{
+	if(ac == 1)
+		prompt(av, env);
+	return (0);
+}
