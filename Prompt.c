39937ef67fd2bd91d6eb3f35328a9d4807722f3e
Elijah <waiganjothiongo12@gmail.com>
2023-11-18 07:47:54 -0800

    Fix

--- Prompt.c
+++ Prompt.c
@@ -1,12 +1,29 @@
+
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 int main(void)
 {
 	char command[1024];
+	char *word;
+	char *args[10];
+	char *env_args[] = {"/bin/bash", (char *)0};
+	int i = 0;
 
 	printf("$ ");
 	fgets(command, sizeof(command), stdin);
-	printf("\n%s\n", command);
 
-	return (0);
+	command[strcspn(command, "\n")] = 0;
+	word = strtok(command, " ");
+	while(word != NULL)
+	{
+		args[i] = word;
+		printf("%s\n", word);
+		word = strtok(NULL, " ");
+		i++;
+	}
+	args[i] = NULL;
+	execve(args[0], args, env_args);
+	return 0;
 }
