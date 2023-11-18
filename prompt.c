914a7305b665018f89bb726936a3f8a306da01e5
Elijah <waiganjothiongo12@gmail.com>
2023-11-18 13:23:25 -0800

    Fix

--- prompt.c
+++ prompt.c
@@ -2,63 +2,30 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/wait.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-
-#define MAX_COMMAND_LENGTH 1024
 
 int main(void)
 {
-	char command[MAX_COMMAND_LENGTH];
+	char command[1024];
 	char *word;
 	char *args[100];
-	pid_t pid;
 	char *env_args[] = {"/bin/bash", (char *)0};
-	struct stat file_status;
 	int i = 0;
 
-	while (1)
+	printf("$ ");
+	fgets(command, sizeof(command), stdin);
+	command[strcspn(command, "\n")] = 0;
+	word = strtok(command, " ");
+	while(word != NULL)
 	{
-		printf("$ ");
-		if (fgets(command, sizeof(command), stdin) == NULL)
-		{
-			break;
-		}
-		command[strcspn(command, "\n")] = 0;
-		word = strtok(command, " ");
-		while (word != NULL)
-		{
-			args[i] = word;
-			word = strtok(NULL, " ");
-			i++;
-		}
-		args[i] = NULL;
-		pid = fork();
-
-		if (pid == -1)
-		{
-			perror("Error forking process");
-			return (1);
-		}
-		else if (pid == 0)
-		{
-			if (stat(args[0], &file_status) == -1)
-			{
-				perror("Error getting file status");
-				return (1);
-			}
-			if (execve(args[0], args, env_args) == -1)
-			{
-				perror("Error executing command");
-				return (1);
-			}
-		}
-		else
-		{
-			wait(NULL);
-			i = 0;
-		}
+		args[i] = word;
+		word = strtok(NULL, " ");
+		i++;
+	}
+	args[i] = NULL;
+	if(execve(args[0], args, env_args) == -1)
+	{
+		perror("Error executing command");
+		return (1);
 	}
 	return (0);
 }
