e538869b032e628037d52950b5b6e238b2ac453f
Elijah <waiganjothiongo12@gmail.com>
2023-11-18 13:13:25 -0800

    Fix

--- prompt.c
+++ prompt.c
@@ -6,9 +6,11 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
+#define MAX_COMMAND_LENGTH 1024
+
 int main(void)
 {
-	char command[1024];
+	char command[MAX_COMMAND_LENGTH];
 	char *word;
 	char *args[100];
 	pid_t pid;
@@ -16,40 +18,47 @@ int main(void)
 	struct stat file_status;
 	int i = 0;
 
-	printf("$ ");
-	fgets(command, sizeof(command), stdin);
-	command[strcspn(command, "\n")] = 0;
-	word = strtok(command, " ");
-	while (word != NULL)
+	while (1)
 	{
-		args[i] = word;
-		word = strtok(NULL, " ");
-		i++;
-	}
-	args[i] = NULL;
-	pid = fork();
+		printf("$ ");
+		if (fgets(command, sizeof(command), stdin) == NULL)
+		{
+			break;
+		}
+		command[strcspn(command, "\n")] = 0;
+		word = strtok(command, " ");
+		while (word != NULL)
+		{
+			args[i] = word;
+			word = strtok(NULL, " ");
+			i++;
+		}
+		args[i] = NULL;
+		pid = fork();
 
-	if (pid == -1)
-	{
-		perror("Error forking process");
-		return (1);
-	}
-	else if (pid == 0)
-	{
-		if (stat(args[0], &file_status) == -1)
+		if (pid == -1)
 		{
-			perror("Error getting file status");
+			perror("Error forking process");
 			return (1);
 		}
-		if (execve(args[0], args, env_args) == -1)
+		else if (pid == 0)
 		{
-			perror("Error executing command");
-			return (1);
+			if (stat(args[0], &file_status) == -1)
+			{
+				perror("Error getting file status");
+				return (1);
+			}
+			if (execve(args[0], args, env_args) == -1)
+			{
+				perror("Error executing command");
+				return (1);
+			}
 		}
-	}
-	else
-	{
-		wait(NULL);
+		else
+		{
+			wait(NULL);
+			i = 0;
+		}
 	}
 	return (0);
 }
