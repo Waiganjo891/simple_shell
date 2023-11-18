a98fe8c5a61e8be953c476c800096678fdb1a393
Elijah <waiganjothiongo12@gmail.com>
2023-11-18 08:10:10 -0800

    Fix

---
+++ Shell.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(void)
+{
+	char command[1024];
+	char *word;
+	char *args[10];
+	char *env_args[] = {"/bin/bash", (char *)0};
+	int i = 0;
+	pid_t pid;
+
+	printf("$ ");
+	fgets(command, sizeof(command), stdin);
+
+	command[strcspn(command, "\n")] = 0;
+	word = strtok(command, " ");
+	while(word != NULL)
+	{
+		args[i] = word;
+		printf("%s\n", word);
+		word = strtok(NULL, " ");
+		i++;
+	}
+	args[i] = NULL;
+	pid = fork();
+	if(pid == 0)
+	{
+		execve(args[0], args, env_args);
+	}
+	else if(pid > 0)
+	{
+		wait(NULL);
+	}
+	else
+	{
+		perror("Error forking");
+	}
+	return (0);
+}
