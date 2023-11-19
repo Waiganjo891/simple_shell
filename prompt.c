bbdff6e273e5b5cff31df934dbd730aa510be7c6
Elijah <waiganjothiongo12@gmail.com>
2023-11-19 12:37:41 -0800

    Fix

--- prompt.c
+++ prompt.c
@@ -2,16 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 /**
  * main - An integer
- * Return: Always (0);
+ * Return: Always (0)
  */
 int main(void)
 {
 	char command[1024];
 	char *word;
 	char *args[100];
+	pid_t pid;
 	char *env_args[] = {"/bin/bash", (char *)0};
+	int status;
 	int i = 0;
 
 	while (1)
@@ -30,11 +34,29 @@ int main(void)
 			i++;
 		}
 		args[i] = NULL;
-		if (execve(args[0], args, env_args) == -1)
+		pid = fork();
+		if (pid == -1)
 		{
-			perror("Error executing command");
+			perror("Error forking process");
 			return (1);
 		}
+		else if (pid == 0)
+		{
+			if (execve(args[0], args, env_args) == -1)
+			{
+				perror("Error executing command");
+				exit(1);
+			}
+		}
+		else
+		{
+			if (waitpid(pid, &status, 0) == -1)
+			{
+				perror("Error waiting for child process");
+				return (1);
+			}
+		}
+		i = 0;
 	}
 	return (0);
 }
