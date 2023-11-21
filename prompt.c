bf4677643cb2a71ff5494e4808b349af5afb089a
Elijah <waiganjothiongo12@gmail.com>
2023-11-21 00:00:49 -0800

    Fix

--- prompt.c
+++ prompt.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 /**
  * main - An integer
  * Return: Always (0)
@@ -13,6 +14,7 @@ int main(void)
 	char command[1024], *word, *args[1024];
 	pid_t child_pid;
 	char *env_args[] = {"/bin/bash", (char *)0};
+	struct stat file_stat;
 	int i = 0, status;
 
 	while (1)
@@ -20,10 +22,19 @@ int main(void)
 		if (isatty(STDIN_FILENO))
 			printf("$ ");
 		if (fgets(command, sizeof(command), stdin) == NULL)
+		{
 			break;
+		}
 		command[strcspn(command, "\n")] = 0;
 		if (strlen(command) == 0)
+		{
 			continue;
+		}
+		if (strcmp(command, "stat") == 0)
+		{
+			printf("Please provide a file path for the stat command.\n");
+			continue;
+		}
 		word = strtok(command, " ");
 		while (word != NULL)
 		{
@@ -40,10 +51,28 @@ int main(void)
 		}
 		else if (child_pid == 0)
 		{
-			if (execve(args[0], args, env_args) == -1)
+			if (strcmp(args[0], "stat") == 0)
 			{
-				perror("Error: ");
-				return (1);
+				if (args[1] == NULL)
+				{
+					printf("Please provide a file path for the stat command.\n");
+					exit(1);
+				}
+				if (stat(args[1], &file_stat) == -1)
+				{
+					perror("Error: ");
+					exit(1);
+				}
+				printf("File size: %ld bytes\n", file_stat.st_size);
+				exit(0);
+			}
+			else
+			{
+				if (execve(args[0], args, env_args) == -1)
+				{
+					perror("Error: ");
+					return (1);
+				}
 			}
 		}
 		else
